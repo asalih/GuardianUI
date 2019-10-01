6ffaffa3e6be6a1a14fac8672d96e6d1e720c1dd
Ahmet Salih <ahmet.salih@windowslive.com>
2019-10-01 13:46:14 +0300

    ssl for

--- Guardian.Domain/SSLHelper.cs
+++ Guardian.Domain/SSLHelper.cs
@@ -10,7 +10,7 @@ namespace Guardian.Domain
     {
         private const string winCmd = "req -newkey rsa:2048 -x509 -nodes -keyout {2}\\{1}.key -new -out {2}\\{1}.crt -subj \"/CN={0}\" -reqexts SAN -extensions SAN -config \"{3}\" -sha256 -days 3650";
 
-        private const string linuxCmd = "req -newkey rsa:2048 -x509 -nodes -keyout {2}\\{1}.key -new -out {2}\\{1}.crt -subj /CN={0} -reqexts SAN -extensions SAN -config {3} sha256 -days 3650";
+        private const string linuxCmd = "openssl req -newkey rsa:2048 -x509 -nodes -keyout {2}\\{1}.key -new -out {2}\\{1}.crt -subj /CN={0} -reqexts SAN -extensions SAN -config {3} sha256 -days 3650";
         //        private const string linuxCmd = "req \\ " +
         //"- newkey rsa:2048 \\" +
         //"-x509 \\" +
@@ -129,7 +129,7 @@ namespace Guardian.Domain
             }
             catch (Exception ex)
             {
-                throw new Exception($"{openSSLDir}/openssl-gen.sh {args}{Environment.NewLine}", ex);
+                throw new Exception($"/bin/bash" + $" -c \"{args}\"" + $"{Environment.NewLine}", ex);
             }
 
             var certCrtPath = Path.Combine(openSSLDir, $"{baseFileName}.crt");
