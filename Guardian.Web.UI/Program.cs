50b4cc94626d601723d5e002902607fa5397c969
Ahmet Salih <ahmet.salih@windowslive.com>
2020-05-19 23:36:33 +0300

    config changed

--- Guardian.Web.UI/Program.cs
+++ Guardian.Web.UI/Program.cs
@@ -13,7 +13,7 @@ namespace Guardian.Web.UI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:5000")
+                .UseUrls("http://*:5000", "https://*:5001")
                 .UseStartup<Startup>();
     }
 }
